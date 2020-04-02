026f8836dc51777d69ff9ef8acd4854d66776dc2
johnnyurosevic <goose@g.ucla.edu>
2020-04-02 14:57:12 -0700

    Scrape Bug Pictures

--- scrape.py
+++ scrape.py
@@ -3,12 +3,18 @@ from urllib.request import urlretrieve
 from os.path import join
 
 with open("fish.html") as fp:
-    soup = BeautifulSoup(fp, 'html5lib')
+    fish_soup = BeautifulSoup(fp, 'html5lib')
+with open("bug.html") as fp:
+    bug_soup = BeautifulSoup(fp, 'html5lib')
 
-table = soup.table.tr.td.table.tbody
+def scrape_table(table, imgdir):
+    for row in table.find_all('tr')[1:]:
+        cells = row.find_all('td')
+        name = cells[0].a.contents[0]
+        imgurl = cells[1].a.get('href')
+        urlretrieve(imgurl, join(imgdir, "".join(name.split()) + '.png'))
 
-for row in table.find_all('tr')[1:]:
-    cells = row.find_all('td')
-    name = cells[0].a.contents[0]
-    imgurl = cells[1].a.get('href')
-    urlretrieve(imgurl, join("images", "".join(name.split()) + '.png'))
+fish_table = fish_soup.table.tr.td.table.tbody
+bug_table = bug_soup.find_all('table')[2].td
+scrape_table(fish_table, join("images","fish"))
+scrape_table(bug_table, join("images","bug"))
