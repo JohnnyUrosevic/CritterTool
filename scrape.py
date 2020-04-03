941cc3fdd43e9060eb8a6a60a346083976686991
johnnyurosevic <goose@g.ucla.edu>
2020-04-02 19:45:19 -0700

    Scrape Other Data about Critters

--- scrape.py
+++ scrape.py
@@ -7,14 +7,34 @@ with open("fish.html") as fp:
 with open("bug.html") as fp:
     bug_soup = BeautifulSoup(fp, 'html5lib')
 
-def scrape_table(table, imgdir):
+def scrape_table(table, fish, fp):
     for row in table.find_all('tr')[1:]:
         cells = row.find_all('td')
         name = cells[0].a.contents[0]
         imgurl = cells[1].a.get('href')
+        imgdir = join("public","images","fish" if fish else "bug")
         urlretrieve(imgurl, join(imgdir, "".join(name.split()) + '.png'))
+        
+        data = name
+        for i in range(2, 4):
+            data += ',' + cells[i].contents[0].strip()
+        data += ',' + cells[4 + int(fish)].small.contents[0].strip()
+
+        # months
+        start = 5 + int(fish)
+        months = []
+        for i in range(start, len(cells)):
+            if cells[i].contents[0].strip() == '✓':
+                months.append(i - start + 1)
+
+        data += ',' + str(months)
+        fp.write(data)
+        break
 
 fish_table = fish_soup.table.tr.td.table.tbody
 bug_table = bug_soup.find_all('table')[2].td
-scrape_table(fish_table, join("images","fish"))
-scrape_table(bug_table, join("images","bug"))
+
+with open('public/fish_data.csv', 'w') as fp:
+    scrape_table(fish_table, True, fp)
+with open('public/bug_data.csv', 'w') as fp:
+    scrape_table(bug_table, False, fp)
