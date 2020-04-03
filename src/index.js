6562c7cac27309bdeb07f53bd521778e19b4e80a
johnnyurosevic <goose@g.ucla.edu>
2020-04-02 19:24:15 -0700

    Moved Images folder and made Critter Component Display Imagae

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Critter from './Critter.js'
 
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello React!</h1>
+      <div>
+        <Critter name="Bitterling" fish={true}/>
+      </div>
     );
   }
 }
