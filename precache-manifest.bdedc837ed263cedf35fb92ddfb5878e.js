d4f6a78d90463f67596342f6d4ef42259a5d645d
johnnyurosevic <goose@g.ucla.edu>
2020-04-05 18:37:53 -0700

    Updates

---
+++ precache-manifest.bdedc837ed263cedf35fb92ddfb5878e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2f1df35fd2f58cd28ad559ccbafd75",
+    "url": "/CritterTool/index.html"
+  },
+  {
+    "revision": "713fb0b4164e471ca5a1",
+    "url": "/CritterTool/static/css/main.82158106.chunk.css"
+  },
+  {
+    "revision": "37039790dc7a5f780ccf",
+    "url": "/CritterTool/static/js/2.daaf27d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CritterTool/static/js/2.daaf27d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713fb0b4164e471ca5a1",
+    "url": "/CritterTool/static/js/main.132b025d.chunk.js"
+  },
+  {
+    "revision": "7b07203206a16c87d5dc",
+    "url": "/CritterTool/static/js/runtime-main.d9e58165.js"
+  }
+]);
